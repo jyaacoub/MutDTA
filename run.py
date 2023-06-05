b1a7f3b4ce7e4a3829ac83af4470773a0a78cffb
jyaacoub <jyaacoub21@gmail.com>
2023-06-05 17:50:23 -0400

    Stats on vina

--- run.py
+++ run.py
@@ -1,5 +1,8 @@
 #%% testing download of sequences from FASTA files
+import matplotlib.pyplot as plt
+from scipy.stats import pearsonr
 import pandas as pd
+import numpy as np
 
 y_path = 'data/PDBbind/kd_ki/Y.csv'
 vina_out = 'results/PDBbind/vina_out/run1.csv'
@@ -10,4 +13,87 @@ actual = pd.read_csv(y_path)
 
 # %%
 mrgd = actual.merge(vina_pred, on='PDBCode')
+y = mrgd['affinity'].values
+z = mrgd['vina_kd(uM)'].values
+log_y = -np.log(y)
+log_z = -np.log(z)
+
+#%% calculating Concordance Index
+def concordance_index(y_true, y_pred):
+    Y = y_true
+    P = y_pred
+    sum = 0
+    num_pairs = 0
+    
+    # loop through all combinations of pairs
+    for i in range(len(Y)):
+        for j in range(i + 1, len(Y)):            
+            if(Y[i] > Y[j]):
+                num_pairs += 1
+                sum +=  1* (P[i] > P[j]) + 0.5 * (P[i] == P[j]) # step function 1 if x > 0, 0.5 if x == 0, 0 if x < 0
+            
+    if num_pairs != 0:
+        print(f"\tnum_pairs: {num_pairs}")
+        return sum/num_pairs
+    else:
+        return 0
+    
+def get_cindex(Y, P):
+    summ = 0
+    pair = 0
+    
+    for i in range(len(Y)):
+        for j in range(i):
+            if i != j:
+                if(Y[i] > Y[j]):
+                    pair += 1
+                    summ +=  1* (P[i] > P[j]) + 0.5 * (P[i] == P[j])
+            else:
+                print('same')
+                break
+        
+            
+    if pair != 0:
+        print(f"\tnum_pairs: {pair}")
+        return summ/pair
+    else:
+        return 0
+    
+# concordance index (This will take a while O(n^2))
+c_index = concordance_index(log_y, log_z)
+print(f"Concordance index: {c_index}")
+print(f'c index: {get_cindex(log_y, log_z)}')
+
+# %% Statistics
+
+# pearson correlation
+p_corr = pearsonr(log_y, log_z)
+print(f"Pearson correlation: {p_corr[0]}")
+print(f"Pearson p-value: {p_corr[1]}")
+
+# error
+mse = np.mean((log_y-log_z)**2)
+mae = np.mean(np.abs(log_y-log_z))
+rmse = np.sqrt(mse)
+print(f"MSE: {mse}")
+print(f"MAE: {mae}")
+print(f"RMSE: {rmse}")
+
+# %% plotting histogram of affinity values
+plt.hist(log_y, bins=10, alpha=0.5)
+plt.hist(log_z, bins=10, alpha=0.5)
+plt.legend(['Experimental', 'Vina'])
+plt.title('Histogram of affinity values (-log(Kd))')
+plt.show()
+
+# scatter plot of affinity values
+# fitting a line
+m, b = np.polyfit(log_y, log_z, 1)
+plt.scatter(log_y, log_z, alpha=0.5)
+plt.plot(log_y, m*log_y + b, color='black', alpha=0.8)
+plt.xlabel('Experimental affinity value')
+plt.ylabel('Vina prediction')
+plt.title('Scatter plot of affinity values (-log(Kd))')
+plt.show()
+
 # %%
