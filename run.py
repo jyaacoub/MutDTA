c44ea964437b79d1fe375911427543ae36327a07
jyaacoub <jyaacoub21@gmail.com>
2023-06-16 16:24:52 -0400

    update, created for all PDBbind

--- run.py
+++ run.py
@@ -1,37 +1,10 @@
-# %%
-from src.models.helpers.contact_map import get_contact
-from tqdm import tqdm
+#%%
 import matplotlib.pyplot as plt
-import os
+import numpy as np
 
-PDBbind = '/cluster/projects/kumargroup/jean/data/refined-set'
-path = lambda c: f'{PDBbind}/{c}/{c}_protein.pdb'
-cmap_p = lambda c: f'{PDBbind}/{c}/{c}_contact_.npy'
-
-# %% main loop to create and save contact maps
-cmaps = {}
-for code in tqdm(os.listdir(PDBbind)[32:100]):
-    if os.path.isdir(os.path.join(PDBbind, code)) and code not in ["index", "readme"]:
-        try:
-            cmap = get_contact(path(code), CA_only=False)
-        except Exception as e:
-            print(code)
-            raise e
-            
-        cmaps[code] = cmap
-        np.save(cmap_p(code), cmap)
-        
-
-#%% Displaying:
-r,c = 10,10
-f, ax = plt.subplots(r,c, figsize=(15, 15))
-i=0
-threshold = None
-for i, code in enumerate(cmaps.keys()):
-    cmap = cmaps[code] if threshold is None else cmaps[code] < threshold
-    ax[i//c][i%c].imshow(cmap)
-    ax[i//c][i%c].set_title(code)
-    ax[i//c][i%c].axis('off')
-    i+=1
-
- # %% Saving ALL contact maps
+# %%sample dgraphdta contact map:
+path = "/cluster/home/t122995uhn/projects/DGraphDTA/data/kiba/pconsc4/O00141.npy"
+cmap = np.load(path)
+plt.matshow(cmap)
+plt.show()
+# %%
--- src/models/helpers/contact_map.py
+++ src/models/helpers/contact_map.py
@@ -103,3 +103,38 @@ def get_contact(pdb_file: str, CA_only=True, check_missing=False,
         plt.show()
         
     return m
+
+if __name__ == "__main__":
+    from tqdm import tqdm
+    import matplotlib.pyplot as plt
+    import numpy as np
+    import os
+
+    PDBbind = '/cluster/projects/kumargroup/jean/data/refined-set'
+    path = lambda c: f'{PDBbind}/{c}/{c}_protein.pdb'
+    cmap_p = lambda c: f'{PDBbind}/{c}/{c}_contact_CA.npy'
+
+    # %% main loop to create and save contact maps
+    cmaps = {}
+    for code in tqdm(os.listdir(PDBbind)[32:100]):
+        if os.path.isdir(os.path.join(PDBbind, code)) and code not in ["index", "readme"]:
+            try:
+                cmap = get_contact(path(code), CA_only=True)
+                cmaps[code] = cmap
+            except Exception as e:
+                print(code)
+                raise e
+            # np.save(cmap_p(code), cmap)
+            
+
+    #%% Displaying:
+    r,c = 10,10
+    f, ax = plt.subplots(r,c, figsize=(15, 15))
+    i=0
+    threshold = None
+    for i, code in enumerate(cmaps.keys()):
+        cmap = cmaps[code] if threshold is None else cmaps[code] < threshold
+        ax[i//c][i%c].imshow(cmap)
+        ax[i//c][i%c].set_title(code)
+        ax[i//c][i%c].axis('off')
+        i+=1
