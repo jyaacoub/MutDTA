fa88d93cbf92c23f1fa8f37ba553a09a3319311c
jyaacoub <jyaacoub21@gmail.com>
2023-06-23 14:23:56 -0400

    comparing stats to vina

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-#%%TODO: 
+#%%TODO: get metrics across different quality pdbs
 import pandas as pd
 import matplotlib.pyplot as plt
 
@@ -8,16 +8,17 @@ import matplotlib.pyplot as plt
 df_res = pd.read_csv('results/model_media/DGraphDTA_stats.csv')[2:]
 df_res.sort_values(by='run', inplace=True)
 
-df_res.loc[-1] = ['vina', 0.68,0,0,0,0,0]
+df_res.loc[-1] = ['vina', 0.68,0.508,0.520,17.812,3.427,4.220]
 
-plt.figure()
-bars = plt.bar(df_res['run'],df_res['cindex'])
-bars[0].set_color('green')
-bars[2].set_color('green')
-bars[-1].set_color('red')
-plt.title('cindex')
-plt.xlabel('run')
-plt.xticks(rotation=30)
-plt.ylim((0.2, 0.8))
-plt.show()
+for col in df_res.columns[1:]:
+    plt.figure()
+    bars = plt.bar(df_res['run'],df_res[col])
+    bars[0].set_color('green')
+    bars[2].set_color('green')
+    bars[-1].set_color('red')
+    plt.title(col)
+    plt.xlabel('run')
+    plt.xticks(rotation=30)
+    # plt.ylim((0.2, 0.8))
+    plt.show()
 #%%
