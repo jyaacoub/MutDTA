6450c0d3d72c0f408674da3785e8ce6fab8494d6
jyaacoub <jyaacoub21@gmail.com>
2023-07-04 16:39:26 -0400

    grid search fn

--- run.py
+++ run.py
@@ -27,72 +27,45 @@ random.seed(RAND_SEED)
 np.random.seed(RAND_SEED)
 torch.manual_seed(RAND_SEED)
 
-# uses pretrained weights for initialization
+# Tune Hyperparameters after grid search
+NUM_EPOCHS = 3
 weight_opt = ['kiba', 'davis', 'random']
-batch_size_opt = [32, 64, 128]
-lr_opt = [0.0001, 0.001, 0.01]
-dropout_opt = [0.1, 0.2, 0.3]
-
-# total permutations: 3*3*3*3 = 81
-# Model Hyperparameters
 WEIGHTS = 'davis'
 BATCH_SIZE = 64
 LEARNING_RATE = 0.001
 DROPOUT = 0.2
 
-NUM_EPOCHS = 3
 
 SAVE_RESULTS = False
 media_save_p = 'results/model_media/figures/'
 csv_file = 'results/model_media/DGraphDTA_stats.csv'
+MODEL_KEY = f'{WEIGHTS}W_{NUM_EPOCHS}E' 
+# {BATCH_SIZE}B_{LEARNING_RATE}LR_{DROPOUT}DO are fixed so not included in model key
 
 # %% loading data
 pdb_dataset = PDBbindDataset(PDB_PROCESSED_DIR, PDB_RAW_DIR)
 
-# train_loader, val_loader, test_loader = train_val_test_split(pdb_dataset, 
-#                      train_split=TRAIN_SPLIT, val_split=VAL_SPLIT,
-#                      shuffle_dataset=True, random_seed=RAND_SEED, 
-#                      batch_size=BATCH_SIZE)
-
+train_loader, val_loader, test_loader = train_val_test_split(pdb_dataset, 
+                    train_split=TRAIN_SPLIT, val_split=VAL_SPLIT,
+                    shuffle_dataset=True, random_seed=RAND_SEED, 
+                    batch_size=BATCH_SIZE)
 
 
 #%% loading model:
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 print(f'Device: {device}')
-model_results = {}
-for WEIGHTS, BATCH_SIZE, \
-    LEARNING_RATE, DROPOUT in \
-    itertools.product(weight_opt, batch_size_opt, \
-                      lr_opt, dropout_opt):
-    MODEL_KEY = f'{WEIGHTS}W_{BATCH_SIZE}B_{LEARNING_RATE}LR_{DROPOUT}DO_{NUM_EPOCHS}E'
-    print(f'\n\n{MODEL_KEY}')
+print(f'\n{MODEL_KEY}')
     
+model = DGraphDTA(dropout=DROPOUT)
+model.to(device)
+assert WEIGHTS in weight_opt, 'WEIGHTS must be one of: kiba, davis, random'
+if WEIGHTS != 'random':
+    model_file_name = f'results/model_checkpoints/prior_work/DGraphDTA_{WEIGHTS}_t2.model'
+    model.load_state_dict(torch.load(model_file_name, map_location=device))
 
-    model = DGraphDTA(dropout=DROPOUT)
-    model.to(device)
-    assert WEIGHTS in weight_opt, 'WEIGHTS must be one of: kiba, davis, random'
-    if WEIGHTS != 'random':
-        model_file_name = f'results/model_checkpoints/prior_work/DGraphDTA_{WEIGHTS}_t2.model'
-        model.load_state_dict(torch.load(model_file_name, map_location=device))
-
-    train_loader, val_loader, test_loader = train_val_test_split(pdb_dataset, 
-                        train_split=TRAIN_SPLIT, val_split=VAL_SPLIT,
-                        shuffle_dataset=True, random_seed=RAND_SEED, 
-                        batch_size=BATCH_SIZE)
-    logs = train(model, train_loader, val_loader, device, 
-            epochs=NUM_EPOCHS, lr=LEARNING_RATE)
-
-    loss, pred, actual = test(model, test_loader, device)
-    model_results[MODEL_KEY] = {'test_loss': loss, 
-                                'train_loss': logs['train_loss'][-1],
-                                'val_loss': logs['val_loss'][-1]}
-
-#%% getting top 5 models
-df = pd.DataFrame.from_dict(model_results, orient='index')
-df.sort_values(by=['test_loss'], inplace=True)
-
-print(df.head(5))
-
+# %% training
+logs = train(model, train_loader, val_loader, device, 
+        epochs=NUM_EPOCHS, lr=LEARNING_RATE)
 
 #%%
 from matplotlib.ticker import MaxNLocator
--- src/models/train_test.py
+++ src/models/train_test.py
@@ -1,6 +1,10 @@
+import itertools
 from tqdm import tqdm
 import torch
 import numpy as np
+
+from src.data_processing.utils import train_val_test_split
+from src.models.prior_work import DGraphDTA
 
 def train(model, train_loader, val_loader, device,
           epochs=10, lr=0.001, **kwargs) -> dict:
@@ -119,3 +123,42 @@ def test(model, test_loader, device):
     print(f"Test Loss: {test_loss:.4f}")
     
     return test_loss, pred, actual
+
+
+
+def grid_search(pdb_dataset, TRAIN_SPLIT=0.8, VAL_SPLIT=0.1, RAND_SEED=42,
+                epoch_opt = [5],
+                weight_opt = ['kiba', 'davis', 'random'],
+                batch_size_opt = [32, 64, 128],
+                lr_opt = [0.0001, 0.001, 0.01],
+                dropout_opt = [0.1, 0.2, 0.3]):
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    print(f'Device: {device}')
+    model_results = {}
+    for WEIGHTS, BATCH_SIZE, \
+        LEARNING_RATE, DROPOUT, NUM_EPOCHS in \
+        itertools.product(weight_opt, batch_size_opt, \
+                        lr_opt, dropout_opt, epoch_opt):
+        MODEL_KEY = f'{WEIGHTS}W_{BATCH_SIZE}B_{LEARNING_RATE}LR_{DROPOUT}DO_{NUM_EPOCHS}E'
+        print(f'\n\n{MODEL_KEY}')
+        
+
+        model = DGraphDTA(dropout=DROPOUT)
+        model.to(device)
+        assert WEIGHTS in weight_opt, 'WEIGHTS must be one of: kiba, davis, random'
+        if WEIGHTS != 'random':
+            model_file_name = f'results/model_checkpoints/prior_work/DGraphDTA_{WEIGHTS}_t2.model'
+            model.load_state_dict(torch.load(model_file_name, map_location=device))
+
+        train_loader, val_loader, test_loader = train_val_test_split(pdb_dataset, 
+                            train_split=TRAIN_SPLIT, val_split=VAL_SPLIT,
+                            shuffle_dataset=True, random_seed=RAND_SEED, 
+                            batch_size=BATCH_SIZE)
+        logs = train(model, train_loader, val_loader, device, 
+                epochs=NUM_EPOCHS, lr=LEARNING_RATE)
+
+        loss, pred, actual = test(model, test_loader, device)
+        model_results[MODEL_KEY] = {'test_loss': loss, 
+                                    'train_loss': logs['train_loss'][-1],
+                                    'val_loss': logs['val_loss'][-1]}
+    return model_results
