a2d1c146c84406e863aee763144a057203147604
jyaacoub <jyaacoub21@gmail.com>
2023-07-04 14:56:24 -0400

    added option to return logs for training loss

--- run.py
+++ run.py
@@ -5,8 +5,8 @@ from tqdm import tqdm
 import pandas as pd
 import numpy as np
 import torch
-from torch.utils.data import SubsetRandomSampler
-from torch_geometric.loader import DataLoader
+
+import matplotlib.pyplot as plt
 
 from src.models.prior_work import DGraphDTA
 from src.data_processing import PDBbindDataset, train_val_test_split
@@ -17,30 +17,32 @@ from src.data_analysis import get_metrics
 PDB_RAW_DIR = '../data/v2020-other-PL/'
 PDB_PROCESSED_DIR = '../data/pytorch_PDBbind/'
 
-# uses pretrained weights for initialization
-WEIGHTS = 'kiba'
-TRAIN = True
-SAVE_RESULTS = False
-
-BATCH_SIZE = 128
-NUM_EPOCHS = 100
-LEARNING_RATE = 0.001
+# Dataset Hyperparameters
 TRAIN_SPLIT= .8 # 80% of data for training
 VAL_SPLIT = .1 # 10% for val and remaining is for testing (10%)
 SHUFFLE_DATA = True
 RAND_SEED=0
 
-MODEL_KEY = f'generalTrained_{WEIGHTS}' if TRAIN else f'pretrained_{WEIGHTS}'
-
 random.seed(RAND_SEED)
 np.random.seed(RAND_SEED)
 torch.manual_seed(RAND_SEED)
+
+# uses pretrained weights for initialization
+WEIGHTS = 'kiba'
+SAVE_RESULTS = False
+
+# Model Hyperparameters
+BATCH_SIZE = 128
+NUM_EPOCHS = 50
+LEARNING_RATE = 0.001
+DROPOUT = 0.2
+MODEL_KEY = f'{WEIGHTS}W_{NUM_EPOCHS}epochs'
 
 #%% loading model:
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 print(f'Device: {device}')
 
-model = DGraphDTA()
+model = DGraphDTA(dropout=DROPOUT)
 model.to(device)
 assert WEIGHTS in ['kiba', 'davis', 'random'], 'WEIGHTS must be one of: kiba, davis, random'
 if WEIGHTS != 'random':
@@ -58,8 +60,22 @@ train_loader, val_loader, test_loader = train_val_test_split(pdb_dataset,
                      batch_size=BATCH_SIZE)
 
 # %% training
-train(model, train_loader, val_loader, device, 
-      epochs=NUM_EPOCHS, lr=LEARNING_RATE)
+logs = train(model, train_loader, val_loader, device, 
+    epochs=NUM_EPOCHS, lr=LEARNING_RATE)
+#%%
+from matplotlib.ticker import MaxNLocator
+ax = plt.figure().gca()
+ax.xaxis.set_major_locator(MaxNLocator(integer=True))
+index = np.arange(1, NUM_EPOCHS+1)
+plt.plot(index, logs['train_loss'], label='train')
+plt.plot(index, logs['val_loss'], label='val')
+plt.legend()
+plt.title(f'{MODEL_KEY} Loss')
+plt.xlabel('Epoch')
+# plt.xticks(range(0,NUM_EPOCHS+1, 2))
+plt.xlim(0, NUM_EPOCHS)
+plt.ylabel('Loss')
+plt.savefig(f'results/model_media/{MODEL_KEY}_loss.png')
 
 # %% testing
 pred, actual = test(model, test_loader, device)
--- src/models/train_test.py
+++ src/models/train_test.py
@@ -3,7 +3,7 @@ import torch
 import numpy as np
 
 def train(model, train_loader, val_loader, device,
-          epochs=10, lr=0.001, **kwargs):
+          epochs=10, lr=0.001, **kwargs) -> dict:
     """
     Training loop for graph models.
     Note that **kwargs is used to pass any additional arguments to the optimizer.
@@ -22,17 +22,24 @@ def train(model, train_loader, val_loader, device,
         number of epochs, by default 10
     `lr` : float, optional
         learning rate, by default 0.001
+        
+    Returns
+    -------
+    dict
+        Dictionary containing training and validation loss for each epoch.
     """
     CRITERION = torch.nn.MSELoss()
     OPTIMIZER = torch.optim.Adam(model.parameters(), lr=lr, **kwargs)
 
+    logs = {'train_loss': [], 'val_loss': []}
+    
     for epoch in range(epochs):
         # Training loop
-        model.train()
-        train_loss = 0.0
         with tqdm(total=len(train_loader), 
                   desc=f"Epoch {epoch+1}/{epochs}", 
                   unit="batch") as progress_bar:
+            model.train()
+            train_loss = 0.0
             for batch_pro, batch_mol in train_loader:
                 batch_pro = batch_pro.to(device)
                 batch_mol = batch_mol.to(device)
@@ -76,6 +83,9 @@ def train(model, train_loader, val_loader, device,
 
         # Print training and validation loss for the epoch
         print(f"Epoch {epoch+1}/{epochs}: Train Loss: {train_loss:.4f}, Val Loss: {val_loss:.4f}")
+        logs['train_loss'].append(train_loss)
+        logs['val_loss'].append(val_loss)
+    return logs
 
 
 def test(model, test_loader, device):
