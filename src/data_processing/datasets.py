037109f7e6fd5d3b3634a41fbc7e697b6630b4a5
jyaacoub <jyaacoub21@gmail.com>
2023-07-16 23:20:37 -0400

    chore(datasets): created `BaseDataset`
    Simplification of new dataset creation + better readability.

--- src/data_processing/datasets.py
+++ src/data_processing/datasets.py
@@ -1,4 +1,3 @@
-from typing import Any, Callable, Optional
 from collections import OrderedDict
 import json, pickle, re, os
 
@@ -10,12 +9,138 @@ from tqdm import tqdm
 
 from src.feature_extraction import smile_to_graph, target_to_graph
 from src.feature_extraction.process_msa import check_aln_lines
-from src.feature_extraction.protein import create_save_cmaps, create_aln_files
+from src.feature_extraction.protein import create_save_cmaps
 from src.data_processing import PDBbindProcessor
 
 #  See: https://pytorch-geometric.readthedocs.io/en/latest/tutorial/create_dataset.html
 # for details on how to create a dataset
-class PDBbindDataset(torchg.data.InMemoryDataset): # InMemoryDataset is used if the dataset is small and can fit in CPU memory
+class BaseDataset(torchg.data.InMemoryDataset):
+    def __init__(self, save_root:str, data_root:str, aln_dir:str,
+                 cmap_threshold:float, shannon=True,  *args, **kwargs):
+        """
+        Base class for datasets. This class is used to create datasets for 
+        graph models. Subclasses only need to define the `pre_process` method
+        and raw_file_names property. The `pre_process` method is used to create
+        an XY.csv file that contains the binding data and the sequences for 
+        proteins and ligands.
+
+        Parameters
+        ----------
+        `save_root` : str, optional
+            Path to processed dir, by default '../data/DavisKibaDataset/'
+        `data_root` : str, optional
+            Path to raw data files, by default '../data/davis_kiba/davis/'
+        `aln_dir` : str, optional
+            Path to sequence alignment directory with files of the name 
+            '{code}_cleaned.a3m'. If set to None then no PSSM calculation is 
+            done and is set to zeros, by default '../data/davis_kiba/davis/aln/'.
+        `cmap_threshold` : float, optional
+            Threshold for contact map creation, DGraphDTA use probability based 
+            cmaps so we use negative to indicate this. (see `feature_extraction.protein.
+            target_to_graph` for details), by default -0.5.
+        `shannon` : bool, optional
+            If True, shannon entropy instead of PSSM matrix is used for 
+            protein features, by default False.
+            
+        *args and **kwargs sent to superclass `torch_geometric.data.InMemoryDataset`.
+        """
+        self.data_root = data_root
+        self.aln_dir =  aln_dir # path to sequence alignments
+        
+        self.cmap_threshold = cmap_threshold
+        self.shannon = shannon
+        
+        super(BaseDataset, self).__init__(save_root, *args, **kwargs)
+        self.load()
+        
+    def cmap_p(self, code):
+        return f'{self.data_root}/pconsc4/{code}.npy'
+    
+    def aln_p(self, code):
+        # see feature_extraction/process_msa.py for details on how the alignments are cleaned
+        if self.aln_dir is None:
+            # dont use aln if none provided (will set to zeros)
+            return None
+        return f'{self.aln_dir}/{code}.aln'
+    
+    @property
+    def processed_file_names(self):
+        # XY.csv cols: PDBCode,pkd,SMILE,prot_seq
+        # XY is created in pre_process
+        return ['XY.csv','data_pro.pt','data_mol.pt']
+    
+    def load(self):
+        self._data_pro = torch.load(self.processed_paths[1])
+        self._data_mol = torch.load(self.processed_paths[2])
+        
+    def __len__(self):
+        return len(self._data_mol)
+    
+    def __getitem__(self, idx):
+        return self._data_pro[idx], self._data_mol[idx]
+    
+    def process(self):
+        """
+        This method is used to create the processed data files after feature extraction.
+        """
+        if not os.path.isfile(self.processed_paths[0]):
+            df = self.pre_process()
+        else:
+            df = pd.read_csv(self.processed_paths[0])
+            print(f'{self.processed_paths[0]} file found, using it to create the dataset')
+        print(f'Number of codes: {len(df)}')
+        
+        # creating the dataset:
+        data_list = []
+        errors = []
+        for idx in tqdm(df.index, 'Extracting node features and creating graphs'):
+            code = df.loc[idx]['code']
+            cmap = np.load(self.cmap_p(code))
+            pro_seq = df.loc[idx]['prot_seq']
+            lig_seq = df.loc[idx]['SMILE']
+            label = df.loc[idx]['pkd']
+            label = torch.Tensor([[label]])
+            
+            _, pro_feat, pro_edge = target_to_graph(pro_seq, cmap, 
+                                                    threshold=self.cmap_threshold,
+                                                    aln_file=self.aln_p(code),
+                                                    shannon=self.shannon)
+            try:
+                _, mol_feat, mol_edge = smile_to_graph(lig_seq)
+            except ValueError:
+                errors.append(code)
+                continue
+            
+            pro = torchg.data.Data(x=torch.Tensor(pro_feat),
+                                edge_index=torch.LongTensor(pro_edge).transpose(1, 0),
+                                y=label,
+                                code=code)
+            lig = torchg.data.Data(x=torch.Tensor(mol_feat),
+                                edge_index=torch.LongTensor(mol_edge).transpose(1, 0),
+                                y=label,
+                                code=code)
+            data_list.append([pro, lig])
+            
+        print(f'{len(errors)} codes failed to create graphs')
+
+        if self.pre_filter is not None:
+            data_list = [data for data in data_list if self.pre_filter(data)]
+        if self.pre_transform is not None:
+            data_list = [self.pre_transform(data) for data in data_list]
+            
+        def collate(data_list):
+            batchA = torchg.data.Batch.from_data_list([data[0] for data in data_list])
+            batchB = torchg.data.Batch.from_data_list([data[1] for data in data_list])
+            return batchA, batchB
+        
+        self._data_pro, self._data_mol = collate(data_list)
+        
+        print('Saving...')
+        torch.save(self._data_pro, self.processed_paths[1])
+        torch.save(self._data_mol, self.processed_paths[2])
+
+
+class PDBbindDataset(BaseDataset): # InMemoryDataset is used if the dataset is small and can fit in CPU memory
     def __init__(self, save_root='../data/PDBbindDataset/msa', 
                  bind_root='../data/v2020-other-PL', 
                  aln_dir=None,
@@ -41,25 +166,10 @@ class PDBbindDataset(torchg.data.InMemoryDataset): # InMemoryDataset is used if 
             protein features, by default False.
             
         *args and **kwargs sent to superclass `torch_geometric.data.InMemoryDataset`.
-        """
-        self.aln_dir =  aln_dir # path to sequence alignments
-        self.bind_dir = bind_root
-        
-        self.cmap_p = lambda code: f'{self.bind_dir}/{code}/{code}_cmap_CB_lone.npy'
-        # see feature_extraction/process_msa.py for details on how the alignments are cleaned
-        
-        if self.aln_dir is None: 
-            # dont use aln if none provided (will set to zeros)
-            self.aln_p = lambda code: None
-        else:
-            self.aln_p = lambda code: f'{self.aln_dir}/{code}_cleaned.a3m'
-        
-        self.cmap_threshold = cmap_threshold
-        self.shannon = shannon
-        
-        
-        super(PDBbindDataset, self).__init__(save_root, *args, **kwargs)
-        self.load()
+        """        
+        super(PDBbindDataset, self).__init__(save_root, data_root=bind_root,
+                                             aln_dir=aln_dir, cmap_threshold=cmap_threshold,
+                                             shannon=shannon, *args, **kwargs)
         
     # for data augmentation override the transform method
     @property
@@ -80,16 +190,6 @@ class PDBbindDataset(torchg.data.InMemoryDataset): # InMemoryDataset is used if 
         """
         return ['index/INDEX_general_PL_data.2020', 
                 'index/INDEX_general_PL_name.2020']
-
-    @property
-    def processed_file_names(self):
-        # XY.csv cols: PDBCode,pkd,SMILE,prot_seq
-        return ['data_pro.pt','data_mol.pt', 'XY.csv']
-
-    def load(self, path: str=None):
-        path = self.processed_dir if path is None else path
-        self._data_pro = torch.load(path + '/data_pro.pt')
-        self._data_mol = torch.load(path + '/data_mol.pt')
         
     def pre_process(self):
         """
@@ -143,73 +243,11 @@ class PDBbindDataset(torchg.data.InMemoryDataset): # InMemoryDataset is used if 
         # merging dataframes:
         df = df_smi[df_smi.SMILE.notna()].merge(df_binding, on='PDBCode')
         df = df.merge(df_seq, on='PDBCode')
-        df.to_csv(self.processed_dir + '/XY.csv')
+        df.to_csv(self.processed_paths[0])
         return df
     
-    def process(self):
-        if not os.path.isfile(self.processed_dir + '/XY.csv'):
-            df = self.pre_process()
-        else:
-            df = pd.read_csv(self.processed_dir + '/XY.csv', index_col='PDBCode')
-            print('XY.csv file found, using it to create the dataset')
-            print(f'Number of codes: {len(df)}')
-        
-        
-        # creating the dataset:
-        data_list = []
-        errors = []
-        for code in tqdm(df.index, 'Extracting node features and creating graphs'):
-            cmap = np.load(self.cmap_p(code))
-            pro_seq = df.loc[code]['prot_seq']
-            lig_seq = df.loc[code]['SMILE']
-            label = df.loc[code]['pkd']
-            label = torch.Tensor([[label]])
-            
-            pro_size, pro_feat, pro_edge = target_to_graph(pro_seq, cmap, 
-                                                           threshold=self.cmap_threshold,
-                                                           aln_file=self.aln_p(code),
-                                                           shannon=self.shannon)
-            try:
-                mol_size, mol_feat, mol_edge = smile_to_graph(lig_seq)
-            except ValueError:
-                errors.append(code)
-                continue
-            
-            pro = torchg.data.Data(x=torch.Tensor(pro_feat),
-                                edge_index=torch.LongTensor(pro_edge).transpose(1, 0),
-                                y=label,
-                                code=code)
-            lig = torchg.data.Data(x=torch.Tensor(mol_feat),
-                                edge_index=torch.LongTensor(mol_edge).transpose(1, 0),
-                                y=label,
-                                code=code)
-            data_list.append([pro, lig])
-            
-        print(f'{len(errors)} codes failed to create graphs')
-
-        if self.pre_filter is not None:
-            data_list = [data for data in data_list if self.pre_filter(data)]
-        if self.pre_transform is not None:
-            data_list = [self.pre_transform(data) for data in data_list]
-            
-        def collate(data_list):
-            batchA = torchg.data.Batch.from_data_list([data[0] for data in data_list])
-            batchB = torchg.data.Batch.from_data_list([data[1] for data in data_list])
-            return batchA, batchB
-        
-        self._data_pro, self._data_mol = collate(data_list)
-        
-        torch.save(self._data_pro, self.processed_dir + '/data_pro.pt')
-        torch.save(self._data_mol, self.processed_dir + '/data_mol.pt')
-
-    def __len__(self):
-        return len(self._data_mol)
-
-    def __getitem__(self, idx):
-        return self._data_pro[idx], self._data_mol[idx]
     
-    
-class DavisKibaDataset(torchg.data.InMemoryDataset):
+class DavisKibaDataset(BaseDataset):
     def __init__(self, save_root='../data/DavisKibaDataset/', 
                  data_root='../data/davis_kiba/davis/', 
                  aln_dir='../data/davis_kiba/davis/aln/',
@@ -237,25 +275,10 @@ class DavisKibaDataset(torchg.data.InMemoryDataset):
             
         *args and **kwargs sent to superclass `torch_geometric.data.InMemoryDataset`.
         """
-        self.data_root = data_root
-        self.aln_dir =  aln_dir # path to sequence alignments
-        
-        self.cmap_p = lambda code: f'{self.data_root}/pconsc4/{code}.npy'
-        # see feature_extraction/process_msa.py for details on how the alignments are cleaned
-        
-        if self.aln_dir is None: 
-            # dont use aln if none provided (will set to zeros)
-            self.aln_p = lambda code: None
-        else:
-            self.aln_p = lambda code: f'{self.aln_dir}/{code}.aln'
-        
-        self.cmap_threshold = cmap_threshold
-        self.shannon = shannon
-
-        super(DavisKibaDataset, self).__init__(save_root, *args, **kwargs)
-        self.load()
-        
-    #TODO: for data augmentation override the transform method
+        super(DavisKibaDataset, self).__init__(save_root, data_root=data_root,
+                                               aln_dir=aln_dir, cmap_threshold=cmap_threshold,
+                                               shannon=shannon, *args, **kwargs)
+    
     @property
     def raw_file_names(self):
         """
@@ -278,16 +301,6 @@ class DavisKibaDataset(torchg.data.InMemoryDataset):
         return [f'{self.data_root}/proteins.txt',
                 f'{self.data_root}/ligands_can.txt',
                 f'{self.data_root}/Y']
-
-    @property
-    def processed_file_names(self):
-        # XY.csv cols: PDBCode,pkd,SMILE,prot_seq
-        return ['data_pro.pt','data_mol.pt', 'XY.csv']
-
-    def load(self, path: str=None):
-        path = self.processed_dir if path is None else path
-        self._data_pro = torch.load(path + '/data_pro.pt')
-        self._data_mol = torch.load(path + '/data_mol.pt')
     
     def pre_process(self):
         """
@@ -348,72 +361,7 @@ class DavisKibaDataset(torchg.data.InMemoryDataset):
             df['pkd'] = [-np.log10(y*1e-9) for y in affinity_mat[lig_r, prot_c]]
         else:
             df['pkd'] = affinity_mat[lig_r, prot_c]
-        df.to_csv(self.processed_dir + '/XY.csv')
+        df.to_csv(self.processed_paths[0])
         return df
-    
-    def process(self):
-        if not os.path.isfile(self.processed_dir + '/XY.csv'):
-            df = self.pre_process()
-        else:
-            df = pd.read_csv(self.processed_dir + '/XY.csv')
-            print('XY.csv file found, using it to create the dataset')
-        print(f'Number of codes: {len(df)}')
-        df = df.loc[:30_000] # cap dataset size (due to memory constraints)
-        
-        
-        # creating the dataset:
-        data_list = []
-        errors = []
-        for idx in tqdm(df.index, 'Extracting node features and creating graphs'):
-            code = df.loc[idx]['code']
-            cmap = np.load(self.cmap_p(code))
-            pro_seq = df.loc[idx]['prot_seq']
-            lig_seq = df.loc[idx]['SMILE']
-            label = df.loc[idx]['pkd']
-            label = torch.Tensor([[label]])
-            
-            _, pro_feat, pro_edge = target_to_graph(pro_seq, cmap, 
-                                                    threshold=self.cmap_threshold,
-                                                    aln_file=self.aln_p(code),
-                                                    shannon=self.shannon)
-            try:
-                _, mol_feat, mol_edge = smile_to_graph(lig_seq)
-            except ValueError:
-                errors.append(code)
-                continue
-            
-            pro = torchg.data.Data(x=torch.Tensor(pro_feat),
-                                edge_index=torch.LongTensor(pro_edge).transpose(1, 0),
-                                y=label,
-                                code=code)
-            lig = torchg.data.Data(x=torch.Tensor(mol_feat),
-                                edge_index=torch.LongTensor(mol_edge).transpose(1, 0),
-                                y=label,
-                                code=code)
-            data_list.append([pro, lig])
-            
-        print(f'{len(errors)} codes failed to create graphs')
-
-        if self.pre_filter is not None:
-            data_list = [data for data in data_list if self.pre_filter(data)]
-        if self.pre_transform is not None:
-            data_list = [self.pre_transform(data) for data in data_list]
-            
-        def collate(data_list):
-            batchA = torchg.data.Batch.from_data_list([data[0] for data in data_list])
-            batchB = torchg.data.Batch.from_data_list([data[1] for data in data_list])
-            return batchA, batchB
-        
-        self._data_pro, self._data_mol = collate(data_list)
-        
-        print('Saving...')
-        torch.save(self._data_pro, self.processed_dir + '/data_pro.pt')
-        torch.save(self._data_mol, self.processed_dir + '/data_mol.pt')
-
-    def __len__(self):
-        return len(self._data_mol)
-
-    def __getitem__(self, idx):
-        return self._data_pro[idx], self._data_mol[idx]
     
     
