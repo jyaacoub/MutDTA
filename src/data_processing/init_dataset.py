60cd8871537e3ed6230389306cd765d0b5efa4bb
jyaacoub <jyaacoub21@gmail.com>
2023-08-21 16:10:34 -0400

    feat(init_dataset): init train_val_test splits

--- src/data_processing/init_dataset.py
+++ src/data_processing/init_dataset.py
@@ -9,14 +9,15 @@ sys.path.append(PROJECT_ROOT)
 
 from src.feature_extraction.process_msa import create_pfm_np_files
 from src.data_processing.datasets import DavisKibaDataset
+from src.train_test.utils import train_val_test_split
 
 if __name__ == "__main__":
     datas = ['davis', 'kiba']
     FEATUREs = ['nomsa', 'msa', 'shannon']
+    data_root_dir = '/cluster/home/t122995uhn/projects/data/' #/home/jyaacoub/projects/data/davis_kiba/
 
     for data, FEATURE in itertools.product(datas, FEATUREs):
-        # DATA_ROOT = f'/home/jyaacoub/projects/data/davis_kiba/{data}/'
-        DATA_ROOT = f'/cluster/home/t122995uhn/projects/data/{data}/'
+        DATA_ROOT = f'{data_root_dir}/{data}/'
         print('\n', data, FEATURE)
         create_pfm_np_files(DATA_ROOT+'/aln/', processes=4)
         if FEATURE == 'nomsa':
@@ -33,6 +34,14 @@ if __name__ == "__main__":
                     aln_dir=f'{DATA_ROOT}/aln/', 
                     cmap_threshold=-0.5, 
                     shannon=FEATURE=='shannon')
+        
+        # saving training, validation, and test sets
+        train_loader, val_loader, test_loader = train_val_test_split(dataset, 
+                                train_split=0.8, val_split=0.1, random_seed=0,
+                                split_by_prot=True)
+        dataset.save_subset(train_loader, 'train')
+        dataset.save_subset(val_loader, 'val')
+        dataset.save_subset(test_loader, 'test')
             
         del dataset # free up memory
                         
