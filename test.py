355d39af08ae3c65ba4fa98b8b803e195c9cb3ea
jyaacoub <jyaacoub21@gmail.com>
2023-08-22 11:05:30 -0400

    feat(test): test script for testing
    This test script is to test models for testing on test datasets with test loaders contained within a script named `test.py`. :P

---
+++ test.py
@@ -0,0 +1,57 @@
+#%%
+import torch
+from torch_geometric.loader import DataLoader
+
+from src.data_analysis.metrics import get_metrics
+from src.train_test.training import train_tune, test
+from src.models.prior_work import DGraphDTA
+from src.models.mut_dta import EsmDTA, EsmAttentionDTA
+from src.utils.loader import Loader
+
+
+
+MODEL = 'DG'
+DATA = 'davis'
+FEATURE = 'nomsa'
+EDGE = 'simple'
+BATCH_SIZE = 16
+LEARNING_RATE = 0.0001
+DROPOUT = 0.4
+EPOCHS = 2000
+checkpoint_p = lambda x: f'results/model_checkpoints/ours/{x}.model_tmp'
+
+media_save_dir = 'results/model_media/'
+media_save_p = f'{media_save_dir}/{DATA}/'
+MODEL_STATS_CSV = 'results/model_media/model_stats.csv'
+
+MODEL_KEY = Loader.get_model_key(MODEL,DATA,FEATURE,EDGE,
+                                     BATCH_SIZE,LEARNING_RATE,DROPOUT,EPOCHS)
+MODEL_KEY = 'DDP-' + MODEL_KEY
+
+# %%
+test_dataset = Loader.load_dataset(DATA, FEATURE, subset='test', path='../data')
+test_loader = DataLoader(test_dataset, 1, shuffle=False)
+
+
+#%%
+model = Loader.load_model(MODEL, FEATURE, EDGE, DROPOUT)
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+# due to https://discuss.pytorch.org/t/check-if-model-is-wrapped-in-nn-dataparallel/67957
+mdl_dict = torch.load(checkpoint_p(MODEL_KEY), map_location=device)
+new_dict = {(k[7:] if 'module.' == k[:7] else k):v for k,v in mdl_dict.items()}
+
+model.load_state_dict(new_dict)
+model.to(device)
+
+#%%
+loss, pred, actual = test(model, test_loader, device)
+print(f'# Test loss: {loss}')
+get_metrics(actual, pred,
+            save_results=True,
+            save_path=media_save_p,
+            model_key=MODEL_KEY,
+            csv_file=MODEL_STATS_CSV,
+            show=False,
+            )
+# %%
