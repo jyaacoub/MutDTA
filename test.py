2161ddeeee5ba20b439f32f902b14cd612bf1bbf
jyaacoub <jyaacoub21@gmail.com>
2023-10-02 11:36:21 -0400

    style: cleanup playground.py and test.py

--- test.py
+++ test.py
@@ -1,6 +1,4 @@
 #%%
-
-#%%
 from src.utils import config
 import torch
 import os
@@ -10,8 +8,6 @@ from src.data_analysis.metrics import get_metrics
 from src.train_test.training import test
 from src.utils.loader import Loader
 from src.utils.arg_parse import parse_train_test_args
-from src.train_test.utils import train_val_test_split
-from src.utils.exceptions import DatasetNotFound
 args = parse_train_test_args(verbose=True,
                              jyp_args=' -m EDI -d PDBbind -f nomsa -e anm -lr 0.0001 -bs 20 -do 0.4 -ne 2000')
 #%%
@@ -73,3 +69,10 @@ get_metrics(actual, pred,
             show=False,
             )
 # %%
+# renaming checkpoint to remove _tmp specification
+model_p = checkpoint_p(MODEL_KEY)
+model_p_tmp = checkpoint_p_tmp(MODEL_KEY)
+
+if (not os.path.isfile(model_p) and  # ensuring no overwrite
+    os.path.isfile(model_p_tmp)):
+    os.rename(model_p_tmp, model_p)
