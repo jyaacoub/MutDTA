1b03573b91fb99fd729278bcd1b304a17b85e062
jyaacoub <jyaacoub21@gmail.com>
2023-10-08 13:01:26 -0400

    fix(DDP): additional params for slurm in submitit

--- train_test_DDP.py
+++ train_test_DDP.py
@@ -10,7 +10,7 @@ from src.train_test import dtrain
 #DGM_kibaD_shannonF_binaryE_64B_0.0001LR_0.4D_2000E
 args = parse_train_test_args(verbose=True, distributed=True,
             jyp_args=' -odir ./slurm_tests/edge_weights/%j'+ \
-                ' -m EDI -d davis -f nomsa -e binary -lr 0.0001 -bs 20 -do 0.4 --train'+ \
+                ' -m DG -d kiba -f nomsa -e anm -lr 0.0001 -bs 32 -do 0.4 --train'+ \
                 ' -s_t 4320 -s_m 10GB -s_nn 1 -s_ng 2') # 3days == 4320 mins
 
 # args = parse_train_test_args(verbose=True, distributed=True,
@@ -31,6 +31,14 @@ print("out_dir:", args.output_dir)
 executor = submitit.AutoExecutor(folder=args.output_dir, 
                                  slurm_max_num_timeout=0) # no requeuing if TIMEOUT or preemption
 
+additional_params = { # slurm params not accepted by submitit
+        'mail-type': 'ALL',
+        'mail-user': 'j.yaacoub@mail.utoronto.ca',
+        # 'dependency': 'afterok:10275728',
+    }
+if args.slurm_nodelist: # Rescrict to certain nodes
+    additional_params["nodelist"] = args.slurm_nodelist
+
 executor.update_parameters(
     cpus_per_task=args.slurm_cpus_per_task, # needs to be at least 2 for dataloader.workers == 2
     timeout_min=args.slurm_time, # SBATCH "-time" param
@@ -45,18 +53,10 @@ executor.update_parameters(
     slurm_mem=args.slurm_mem,
     # Might need this since ESM takes up a lot of memory
     # slurm_constraint='gpu16g', # using small batch size will be sufficient for now
-    slurm_constraint='gpu32g', # using small batch size will be sufficient for now
+    # slurm_constraint='gpu32g', # using small batch size will be sufficient for now
     # v100-34G can handle batch size of 15 -> v100-16G == 7?
-    additional_parameters={
-        'mail-type': 'ALL',
-        'mail-user': 'j.yaacoub@mail.utoronto.ca',
-        # 'dependency': 'afterany:10258245'
-    }
+    slurm_additional_parameters=additional_params
 )
-
-if args.slurm_nodelist: # Rescrict to certain nodes
-    executor.update_parameters(
-        slurm_additional_parameters={"nodelist": f'{args.slurm_nodelist}' })
 
 #%% submit job:
 job = executor.submit(dtrain, args)
