0481f8c7ae550a91fa77d4f8e9fdfd431a54304f
jyaacoub <jyaacoub21@gmail.com>
2023-11-08 20:47:33 -0500

    Add function to run Foldseek for structural sequence extraction

--- src/feature_extraction/protein_nodes.py
+++ src/feature_extraction/protein_nodes.py
@@ -72,6 +72,62 @@ def residue_features(residue):
             ResInfo.hydrophobic_ph7[residue]]
     return np.array(feats)
 
+def run_foldseek(foldseek,
+                  path,
+                  chains: list = None,
+                  process_id: int = 0,
+                  plddt_path: str = None,
+                  plddt_threshold: float = 70.) -> dict:
+    """
+    
+    Args:
+        foldseek: Binary executable file of foldseek
+        path: Path to pdb file
+        chains: Chains to be extracted from pdb file. If None, all chains will be extracted.
+        process_id: Process ID for temporary files. This is used for parallel processing.
+        plddt_path: Path to plddt file. If None, plddt will not be used.
+        plddt_threshold: Threshold for plddt. If plddt is lower than this value, the structure will be masked.
+
+    Returns:
+        seq_dict: A dict of structural seqs. The keys are chain IDs. The values are tuples of
+        (seq, struc_seq, combined_seq).
+    """
+    assert os.path.exists(foldseek), f"Foldseek not found: {foldseek}"
+    assert os.path.exists(path), f"Pdb file not found: {path}"
+    assert plddt_path is None or os.path.exists(plddt_path), f"Plddt file not found: {plddt_path}"
+    
+    tmp_save_path = f"get_struc_seq_{process_id}.tsv"
+    cmd = f"{foldseek} structureto3didescriptor -v 0 --threads 1 --chain-name-mode 1 {path} {tmp_save_path}"
+    os.system(cmd)
+
+    seq_dict = {}
+    name = os.path.basename(path)
+    with open(tmp_save_path, "r") as r:
+        for i, line in enumerate(r):
+            desc, seq, struc_seq = line.split("\t")[:3]
+            
+            # Mask low plddt
+            if plddt_path is not None:
+                with open(plddt_path, "r") as r:
+                    plddts = np.array(json.load(r)["confidenceScore"])
+                    
+                    # Mask regions with plddt < threshold
+                    indices = np.where(plddts < plddt_threshold)[0]
+                    np_seq = np.array(list(struc_seq))
+                    np_seq[indices] = "#"
+                    struc_seq = "".join(np_seq)
+            
+            name_chain = desc.split(" ")[0]
+            chain = name_chain.replace(name, "").split("_")[-1]
+
+            if chains is None or chain in chains:
+                if chain not in seq_dict:
+                    combined_seq = "".join([a + b.lower() for a, b in zip(seq, struc_seq)])
+                    seq_dict[chain] = (seq, struc_seq, combined_seq)
+        
+    os.remove(tmp_save_path)
+    os.remove(tmp_save_path + ".dbtype")
+    return seq_dict
 
 if __name__ == '__main__':
     # dir_p = '/home/jyaacoub/projects/data/msa/outputs'
