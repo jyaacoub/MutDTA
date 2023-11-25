5bee8d3a3565c9ad23c8805f15a0bed40b4fda33
jyaacoub <jyaacoub21@gmail.com>
2023-11-25 15:03:56 -0500

    style(fig6): darkgrid to be consistent

--- playground.py
+++ playground.py
@@ -1,7 +1,10 @@
 #%%
 import matplotlib.pyplot as plt
+import seaborn as sns
 from src.data_analysis.figures import fig6_protein_appearance
 
+
+sns.set(style="darkgrid")
 fig6_protein_appearance()
 plt.savefig('results/figures/fig6_protein_appearance_dataset.png', dpi=300)
 
