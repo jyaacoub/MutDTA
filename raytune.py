73b4d5613e49121be6fb09e49f865a444b8fbef0
jyaacoub <jyaacoub21@gmail.com>
2023-12-05 21:15:15 -0500

    feat(raytune): init tuner for simple models

---
+++ raytune.py
@@ -0,0 +1,77 @@
+# This is a simple tuning script for the raytune library.
+# no support for distributed training in this file.
+
+import torch
+from ray.air import session
+from ray.train.torch import TorchCheckpoint
+from ray import tune
+from ray.tune.search import ConcurrencyLimiter
+from ray.tune.search.bayesopt import BayesOptSearch
+
+
+from src.utils.loader import Loader
+from src.train_test.tune import simple_train, simple_test
+from src.utils import config as cfg
+
+
+def objective(config):
+    save_checkpoint = config.get("save_checkpoint", False)
+    loaders = Loader.load_DataLoaders(data=config['dataset'], pro_feature=config['feature_opt'], 
+                                      edge_opt=config['edge_opt'], 
+                                      path=cfg.DATA_ROOT, 
+                                      batch_train=config['batch_size'],
+                                      datasets=['train', 'val'],
+                                      training_fold=config['fold_selection'])
+    
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    
+    model = Loader.init_model(model=config["model"], pro_feature=config["feature_opt"],
+                              pro_edge=config["edge_opt"], dropout=config["dropout"]
+                              # WARNING: no ligand features for now
+                              ).to(device)
+    
+    optimizer = torch.optim.Adam(model.parameters(), lr=config["lr"])
+    
+    for _ in range(config["epochs"]):
+        simple_train(model, optimizer, loaders['train'], 
+                     device=device, 
+                     epochs=1)  # Train the model
+        loss = simple_test(model, loaders['test'])  # Compute test accuracy
+        
+        checkpoint = None
+        if save_checkpoint:
+            checkpoint = TorchCheckpoint.from_state_dict(model.state_dict())
+            
+        # Report metrics (and possibly a checkpoint) to Tune
+        session.report({"mean_loss": loss}, checkpoint=checkpoint)
+
+algo = BayesOptSearch(utility_kwargs={"kind": "ucb", "kappa": 2.5, "xi": 0.0})
+# algo = ConcurrencyLimiter(algo, max_concurrent=4)
+search_space = {
+    # constants:
+    "epochs": 15, # 15 epochs
+    "model": "DG",
+    "dataset": "davis",
+    "feature_opt": "nomsa",
+    "edge_opt": "nomsa",
+    "fold_selection": 0,
+    "save_checkpoint": False,
+    
+    # hyperparameters to tune:
+    "lr": tune.loguniform(1e-4, 1e-2),
+    "dropout": tune.uniform(0, 0.5),
+    "batch_size": tune.choice([16, 32, 64, 128]),
+}
+
+tuner = tune.Tuner(
+    tune.with_resources(objective, resources={"cpu": 4, "gpu": 1}),
+    tune_config=tune.TuneConfig(
+        metric="mean_loss",
+        mode="min",
+        search_alg=algo,
+        num_samples=50,
+    ),
+    param_space=search_space,
+)
+
+results = tuner.fit()
---
+++ src/train_test/tune.py
@@ -0,0 +1,100 @@
+from typing import Tuple
+
+import numpy as np
+import torch
+from torch_geometric.loader import DataLoader
+
+from src.models.utils import BaseModel
+
+
+def simple_train(model: BaseModel, optimizer:torch.optim.Optimizer, 
+                 train_loader:DataLoader, device: torch.device,
+                 epochs=10) -> dict:
+    """
+    simple training loop without any checkpointing, lr scheduling, early stopping, 
+    or support for distributed training.
+
+    Parameters
+    ----------
+    `model` : BaseModel
+        Model to be trained.
+    `optimizer` : torch.optim
+        Optimizer to use.
+    `train_loader` : DataLoader
+        Data loader for training set.
+    `device` : torch.device
+        Device to train on.
+    `epochs` : int, optional
+        number of epochs, by default 10
+        
+    Returns
+    -------
+    dict
+        Dictionary containing training and validation loss for each epoch.
+    """
+    CRITERION = torch.nn.MSELoss()
+    
+    model.train()
+    for _ in range(epochs):
+        # Training loop
+        for data in train_loader:
+            batch_pro = data['protein'].to(device)
+            batch_mol = data['ligand'].to(device)
+            labels = data['y'].reshape(-1,1).to(device)
+            
+            # Forward pass
+            predictions = model(batch_pro, batch_mol)
+
+            # Compute loss
+            loss = CRITERION(predictions, labels)
+            train_loss += loss.item()
+
+            # Backward pass and optimization
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+
+def simple_test(model:BaseModel, test_loader:DataLoader, device:torch.device, 
+                CRITERION:torch.nn.Module=None) -> float:
+    """
+    Run inference on the test set.
+
+    Parameters
+    ----------
+    `model` : BaseModel
+        The model to run inference on.
+    `test_loader` : DataLoader
+        The test set data loader.
+    `device` : torch.device
+        Device to run inference on.
+    `CRITERION` : torch.nn, optional
+        Loss function, by default None
+
+    Returns
+    -------
+    Tuple[float, np.ndarray, np.ndarray]
+        Tuple of test loss, predictions, and actual labels.
+    """
+    # After training, you can evaluate the model on the test set if needed
+    model.eval()
+    test_loss = 0.0
+    CRITERION = CRITERION or torch.nn.MSELoss()
+    
+    with torch.no_grad():
+        for data in test_loader:
+            batch_pro = data['protein'].to(device)
+            batch_mol = data['ligand'].to(device)
+            labels = data['y'].reshape(-1,1).to(device)
+            
+            # Forward pass
+            predictions = model(batch_pro, batch_mol)
+
+            # Compute loss
+            loss = CRITERION(predictions, labels) # y is labels
+            test_loss += loss.item()
+
+        # Compute average test loss
+        test_loss /= len(test_loader)
+    
+    return test_loss
