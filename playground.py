c02ecd3274496c763842813fc566467b284c6c2c
jyaacoub <jyaacoub21@gmail.com>
2024-03-25 16:16:38 -0400

    feat(figures): figure5 now includes ring3 and aflow #84 #77

--- playground.py
+++ playground.py
@@ -1,40 +1,17 @@
-# #%%
-# from src.data_prep.init_dataset import create_datasets
-# from src import config as cfg
+#%%
+from src.analysis.figures import prepare_df
 
-# create_datasets([cfg.DATA_OPT.PDBbind], [cfg.PRO_FEAT_OPT.nomsa],
-#                 [cfg.PRO_EDGE_OPT.aflow], k_folds=5)
-
-# %% test aflow dataset on DG model
-from src import config as cfg
-from src.utils.loader import Loader
-from src.models.prior_work import DGraphDTA
-
-
-
-mdl: DGraphDTA = Loader.init_model("DG", "nomsa", cfg.PRO_EDGE_OPT.aflow, 0.5)
-db = Loader.load_DataLoaders(cfg.DATA_OPT.PDBbind, cfg.PRO_FEAT_OPT.nomsa, cfg.PRO_EDGE_OPT.aflow,
-                             datasets=['test'], batch_train=1)
-sample = next(iter(db['test']))
+df = prepare_df()
 
 # %%
-p = sample['protein']
-out = mdl.pro_conv1(p.x, p.edge_index, p.edge_weight[:,1]) # edge weight is incorrect shape! must be [E,1] and not [E,6]
+df[df.edge.str.contains('ring3') | df.edge.str.contains('aflow')]
 
+#%%
+from src.analysis.figures import fig5_edge_feat_violin
 
-# %%
-from tqdm import tqdm
-mdl.train()
+fig5_edge_feat_violin(df, sel_dataset='PDBbind', exclude=['simple'], 
+                        sel_col='cindex', show=True, add_stats=False)
+fig5_edge_feat_violin(df, sel_dataset='PDBbind', exclude=['simple'], 
+                        sel_col='mse', show=True, add_stats=False)
 
-for sample in tqdm(db['test']):
-    mdl.forward(sample['protein'], sample['ligand'])
-
-# %%
-import numpy as np
-
-v0 = np.load('../data/pdbbind/v2020-other-PL/edge_weights/aflow/1eub.npy')
-v1 = np.load('../data/pdbbind/v2020-other-PL/edge_weights/aflow_ring3/1eub.npy')
-
-# %%
-(v0 == v1).all()
 # %%
