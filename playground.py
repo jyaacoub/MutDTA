f6cd150faeb5de578a838fe275b4def7a63e07ff
jyaacoub <jyaacoub21@gmail.com>
2024-04-15 10:09:21 -0400

    results: reran aflow model after more hyperparam tuning

--- playground.py
+++ playground.py
@@ -9,7 +9,7 @@ create_datasets(cfg.DATA_OPT.platinum,
                 ligand_edges=cfg.LIG_EDGE_OPT.binary,
                 k_folds=None,
 # just a test dataset, no training can be done on this
-                train_split=0.6, val_split=0.0) 
+                train_split=0.0, val_split=0.0) 
 
 #%%
 import pandas as pd
