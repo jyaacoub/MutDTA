69add713bda16c9f39fc8f874a0fff6f92b94314
jyaacoub <jyaacoub21@gmail.com>
2024-07-08 15:31:34 -0400

    feat(splits): created kiba and pdbind splits #113

--- playground.py
+++ playground.py
@@ -19,3 +19,36 @@ db_aflow = Loader.load_dataset('../data/DavisKibaDataset/davis/nomsa_aflow_origi
 db = Loader.load_dataset('../data/DavisKibaDataset/davis/nomsa_binary_original_binary/full')
 
 # %%
+# 5-fold cross validation + test set
+import pandas as pd
+from src import cfg
+from src.train_test.splitting import balanced_kfold_split
+from src.utils.loader import Loader
+test_df = pd.read_csv('/cluster/home/t122995uhn/projects/MutDTA/splits/pdbbind_test.csv')
+test_prots = set(test_df.prot_id)
+db = Loader.load_dataset(f'{cfg.DATA_ROOT}/PDBbindDataset/nomsa_binary_original_binary/full/')
+
+train, val, test = balanced_kfold_split(db,
+                k_folds=5, test_split=0.1, val_split=0.1, 
+                test_prots=test_prots, random_seed=0, verbose=True
+                )
+
+#%%
+db.save_subset_folds(train, 'train')
+db.save_subset_folds(val, 'val')
+db.save_subset(test, 'test')
+
+#%%
+import shutil, os
+
+src = "/cluster/home/t122995uhn/projects/data/PDBbindDataset/nomsa_binary_original_binary/"
+dst = "/cluster/home/t122995uhn/projects/MutDTA/splits/pdbbind"
+os.makedirs(dst, exist_ok=True)
+
+for i in range(5):
+    sfile = f"{src}/val{i}/XY.csv"
+    dfile = f"{dst}/val{i}.csv"
+    shutil.copyfile(sfile, dfile)
+    
+
+# %%
