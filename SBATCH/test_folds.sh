108c53efd725fc16b196b2a95c66cf7bb2f04969
jyaacoub <jyaacoub21@gmail.com>
2025-02-07 11:35:05 -0500

    chore(results): preds for davis_aflow on #narval #147

--- SBATCH/test_folds.sh
+++ SBATCH/test_folds.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH -t 40 
-#SBATCH --job-name=test_kiba_GVPL
+#SBATCH --job-name=test_davis_aflow
 #SBATCH --mem=10G
 
 #SBATCH --mail-type=ALL
@@ -17,40 +17,20 @@ cd /home/jyaacoub/projects/def-sushant/jyaacoub/MutDTA/
                     
 source .venv/bin/activate
 
-python -u test.py --model_opt GVPL \
-                    --data_opt kiba \
+python -u test.py --model_opt DG \
+                    --data_opt davis \
                      \
                      --feature_opt nomsa \
                      --edge_opt aflow \
-                     --ligand_feature_opt gvp \
+                     --ligand_feature_opt original \
                      --ligand_edge_opt binary \
                      \
-                     --learning_rate 0.00005480618584919115 \
-                     --batch_size 32 \
-                     --dropout 0.0808130125360696 \
-                     --output_dim 512 \
-                     --num_GVPLayers 4 \
+                     --learning_rate 0.0008279387625584954 \
+                     --batch_size 128 \
+                     --dropout 0.3480347297724069 \
+                     --output_dim 256 \
                      \
-                     --train \
-                     --fold_selection ${SLURM_ARRAY_TASK_ID} \
-                     --num_epochs 2000
+                     --save_pred_test \
+                     --fold_selection ${SLURM_ARRAY_TASK_ID} 
 
-# kiba_GVPL only 
-python -u test.py --model_opt GVPL \
-                      --data_opt kiba \
-                     \
-                     --feature_opt nomsa \
-                     --edge_opt binary \
-                     --ligand_feature_opt gvp \
-                     --ligand_edge_opt binary \
-                     \
-                     --learning_rate 0.00003372637625954074 \
-                     --batch_size 32 \
-                     --dropout 0.09399264336737133 \
-                     --output_dim 512 \
-                     --num_GVPLayers 4 \
-                     \
-                     --train \
-                     --fold_selection ${SLURM_ARRAY_TASK_ID} \
-                     --num_epochs 2000
 
